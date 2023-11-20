715d4e44e5944b2fa0f8a8e1dfef1015937de4a8
root <root@ji-jupyter-7382820602863255552-master-0.ji-jupyter-7382820602863255552.default.svc.cluster.local>
2023-11-20 09:22:28 +0000

    add run vqa

---
+++ run_vqa.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+import random
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import json
+
+from transformers import StoppingCriteriaList
+
+from minigpt4.common.config import Config
+from minigpt4.common.dist_utils import get_rank
+from minigpt4.common.registry import registry
+from minigpt4.conversation.conversation import Chat, CONV_VISION_Vicuna0, CONV_VISION_LLama2, StoppingCriteriaSub
+
+# imports modules for registration
+from minigpt4.datasets.builders import *
+from minigpt4.models import *
+from minigpt4.processors import *
+from minigpt4.runners import *
+from minigpt4.tasks import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Demo")
+    parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
+    parser.add_argument("--input-path", required=True, help="path to input json file.")
+    parser.add_argument("--img-dir", required=True, help="directory to image path.")
+    parser.add_argument("--output-path", required=True, help="path to output json file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
+    parser.add_argument(
+        "--options",
+        nargs="+",
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file (deprecate), "
+        "change to --cfg-options instead.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def setup_seeds(config):
+    seed = config.run_cfg.seed + get_rank()
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
+
+# ========================================
+#             Model Initialization
+# ========================================
+
+conv_dict = {'pretrain_vicuna0': CONV_VISION_Vicuna0,
+             'pretrain_llama2': CONV_VISION_LLama2}
+
+print('Initializing Chat')
+args = parse_args()
+cfg = Config(args)
+
+model_config = cfg.model_cfg
+model_config.device_8bit = args.gpu_id
+model_cls = registry.get_model_class(model_config.arch)
+model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+
+CONV_VISION = conv_dict[model_config.model_type]
+
+vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
+vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+
+stop_words_ids = [[835], [2277, 29937]]
+stop_words_ids = [torch.tensor(ids).to(device='cuda:{}'.format(args.gpu_id)) for ids in stop_words_ids]
+stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+
+chat = Chat(model, vis_processor, device='cuda:{}'.format(args.gpu_id), stopping_criteria=stopping_criteria)
+print('Initialization Finished')
+
+
+def ask_llm(img_path, query):
+    chat_state = CONV_VISION.copy()
+    img_list = []
+    chat.upload_img(img_path, chat_state, img_list)
+    chat.encode_img(img_list)
+    chat.ask(query, chat_state)
+    ans = chat.answer(conv=chat_state, img_list=img_list, num_beams=1, temperature=1.0, max_new_tokens=300, max_length=2000)[0]
+    #print ("Query: {}\nAnswer: {}".format(query, ans))
+    return ans
+
+output = []
+img_dir = args.img-dir
+cnt = 0
+with open(args.input-path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+    for item in data:
+        cnt += 1
+        if cnt % 100 == 0 : print (cnt)
+        img_path = os.path.join(img_dir, item["image"])
+        query = item["question"]
+        pred = ask_llm(img_path, query)
+        item["pred"] = pred
+        output.append(item)
+with open(args.output-path, "w") as f:
+    json.dump(output, f, ident=2)
+
+
+#img_path = "/work/imgs/bike-3.png"
+#query = "Answer the question with only a word or a phrase: How many people riding bikes are there?"
+#ask_llm(img_path, query)
+#query = "只用一个词或一个短语回答以下问题：有几个骑自行车的人？"
+#ask_llm(img_path, query)
+#query = "有几个骑自行车的人？"
+#ask_llm(img_path, query)
+#query = "How many people riding bikes are there?"
+#ask_llm(img_path, query)
+
